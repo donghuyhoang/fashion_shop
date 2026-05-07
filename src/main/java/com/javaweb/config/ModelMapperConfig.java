0a3ba9ab55c82d5b2e72e7af1f9532b42af9b7c1
NguyenVanTuyen00 <ngvantuyen00@gmail.com>
2026-05-07 19:32:04 +0700

    Sửa lỗi Kiến trúc - Sử dụng ModelMapper nhưng không đồng nhất quy tắc đặt tên biến

--- src/main/java/com/javaweb/config/ModelMapperConfig.java
+++ src/main/java/com/javaweb/config/ModelMapperConfig.java
@@ -1,14 +1,26 @@
 package com.javaweb.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
-	@Bean
-	public ModelMapper molderMapper() {
-		return new ModelMapper();
-	}
-	
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        
+        /* * Cấu hình MatchingStrategy là STANDARD để ModelMapper có thể tự động ánh xạ 
+         * giữa các biến có quy tắc đặt tên khác nhau (ví dụ: user_id trong Entity 
+         * và userId trong DTO).
+         */
+        mapper.getConfiguration()
+              .setMatchingStrategy(MatchingStrategies.STANDARD)
+              .setFieldMatchingEnabled(true)
+              .setSkipNullEnabled(true);
+              
+        return mapper;
+    }
 }
